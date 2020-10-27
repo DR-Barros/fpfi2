ff08bbdd472492f88ac4762f80c11d4c515b7850
Daniel Radrigan Barros <danielradriganb@gmail.com>
2020-10-27 15:51:41 -0300

    v20.10.21.00 portfolio

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const IMAGE = document.getElementById("Hero-image");
+const BRASILTECA_IMAGE = document.getElementById("img-brasilteca");
 let i = 1;
 
 setTimeout(CambiarImagen, 6000);
@@ -6,12 +7,15 @@ setTimeout(CambiarImagen, 6000);
 function CambiarImagen(){
     if(i === 1){
         IMAGE.src = "img/ejemplo2.jpg";
+        BRASILTECA_IMAGE.src = "img/portfolio/brasilteca/brasilteca_1.jpg"; 
         i++;
     } else if (i === 2){
         IMAGE.src = "img/ejemplo3.jpg";
+        BRASILTECA_IMAGE.src = "img/portfolio/brasilteca/brasilteca_2.jpg"; 
         i++;
     } else {
         IMAGE.src = "img/ejemplo.jpg";
+        BRASILTECA_IMAGE.src = "img/portfolio/brasilteca/brasilteca_0.jpg"; 
         i = 1;
     }
     setTimeout(CambiarImagen, 6000);
