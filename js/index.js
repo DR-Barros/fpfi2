36ce539daad3336a016c55f61f7acf83c52359e8
Daniel Radrigan Barros <danielradriganb@gmail.com>
2020-11-22 17:58:46 -0300

    v20.11.22 brasilteca

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const IMAGE = document.getElementById("Hero-image");
-const BRASILTECA_IMAGE = document.getElementById("img-brasilteca");
 let i = 1;
 
 setTimeout(CambiarImagen, 6000);
@@ -7,15 +6,12 @@ setTimeout(CambiarImagen, 6000);
 function CambiarImagen(){
     if(i === 1){
         IMAGE.src = "img/ejemplo2.jpg";
-        BRASILTECA_IMAGE.src = "img/portfolio/brasilteca/brasilteca_1.jpg"; 
         i++;
     } else if (i === 2){
         IMAGE.src = "img/ejemplo3.jpg";
-        BRASILTECA_IMAGE.src = "img/portfolio/brasilteca/brasilteca_2.jpg"; 
         i++;
     } else {
-        IMAGE.src = "img/ejemplo.jpg";
-        BRASILTECA_IMAGE.src = "img/portfolio/brasilteca/brasilteca_0.jpg"; 
+        IMAGE.src = "img/ejemplo.jpg"; 
         i = 1;
     }
     setTimeout(CambiarImagen, 6000);
